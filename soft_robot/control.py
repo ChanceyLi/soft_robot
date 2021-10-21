480d280211853ca1d2717156a62e5581685c1c92
Chancey_Li <Chancey_Li@outlook.com>
2021-10-21 20:39:18 +0800

    some details about serial part and control part

--- soft_robot/control.py
+++ soft_robot/control.py
@@ -2,6 +2,7 @@ import my_serial
 import model
 import math
 from time import sleep
+
 """
     控制六轴机器人以及软体部分的py文件。
 """
@@ -14,8 +15,32 @@ class Control(my_serial.SerialsMng):
         self.m = model.Model([0, 0, 0, 0, 0, 0], delta=0.1)
 
     def send_and_read(self, theta, delta):
+        """
+        :param theta 六轴机器人角度 delta 软体部分的变量
+        :提供控制量并将机器人移动到目标位置
+        """
         self.m.set_radian(theta, delta)
 
         theta = []
         for i in range(7):
             theta.append(int(self.m.get_radian() * 180 / math.pi))
+        run_gcode = 'G01 A' + str(theta[0]) + ' B' + str(theta[1]) + ' C' + str(theta[2]) + ' D' + str(
+            theta[3]) + ' X' + \
+                    str(theta[4]) + ' F2000\n'
+        run_gcode6 = 'M13.' + str(theta[5]).rjust(3, '0') + '\n'
+        self.setdataAndsend(0, run_gcode)
+        self.setdataAndsend(1, run_gcode6)
+        return self.read_data()
+
+    def auto_run(self, thetas, deltas):
+        """
+        :param thetas, deltas 一系列的角度信息
+        :自动运行一系列的位置移动
+        """
+        for theta, delta in thetas, deltas:
+            res = self.send_and_read(theta, delta)
+            if res[0] == '%':
+                continue
+            else:
+                pass
+            sleep(1)
